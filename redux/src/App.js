52916e2742e460b0f2c75c97f3d110b31f3d85fe
sapey <alexandre.froment92@gmail.com>
2022-03-03 11:49:52 +0100

    reduc

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,8 +1,8 @@
-
-
 import {useSelector , useDispatch} from "react-redux";
 import { MODIF_NOM_USER , modifUserName } from "./actions/user";
-import {DELETE_FIRST} from "./actions/article"
+import {deleteFirst} from "./actions/article"
+import { Form } from "./composants/Form";
+import { FormCompetence } from "./composants/FormCompetence";
 
 function App() {
   const articles = useSelector((state) => { return state.articleReducer})
@@ -17,7 +17,7 @@ function App() {
     dispatch(modifUserName())
   }
   function supprPremier(){
-    dispatch({type:DELETE_FIRST})
+    dispatch(deleteFirst())
   }
 
   console.log(user);
@@ -27,10 +27,15 @@ function App() {
         <h1>Découverte de Redux</h1>
       </header>
       <button onClick={modifNomUser}>Changer le nom de l'utilisateur</button>
+      <FormCompetence />
       <pre>
+        {user?.competences != null && JSON.stringify([...user.competences.values() ], null , " ")}
         {JSON.stringify(user , null , " ")}
       </pre>
       <button onClick={supprPremier}>suppr premier article</button>
+      <hr/>
+        <Form />
+      <hr/>
       <main>
         {articles.length > 0 && <>
           {articles.map((article, index) => {
