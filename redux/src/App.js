9b02e42225a818d91ce4a52c51361a81ebbafbb4
sapey <alexandre.froment92@gmail.com>
2022-03-03 10:06:59 +0100

    start jour4

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,18 +1,36 @@
 
-import {useSelector} from "react-redux";
+
+import {useSelector , useDispatch} from "react-redux";
+import { MODIF_NOM_USER , modifUserName } from "./actions/user";
+import {DELETE_FIRST} from "./actions/article"
 
 function App() {
   const articles = useSelector((state) => { return state.articleReducer})
   const user = useSelector((state) => {return state.userReducer})
+  const dispatch = useDispatch()
+
+  /* function modifNomUser(){
+    dispatch({type:MODIF_NOM_USER , payload : "Alain"})
+  } */
+
+  function modifNomUser(){
+    dispatch(modifUserName())
+  }
+  function supprPremier(){
+    dispatch({type:DELETE_FIRST})
+  }
+
   console.log(user);
   return (
     <div className="App">
       <header>
         <h1>Découverte de Redux</h1>
       </header>
+      <button onClick={modifNomUser}>Changer le nom de l'utilisateur</button>
       <pre>
         {JSON.stringify(user , null , " ")}
       </pre>
+      <button onClick={supprPremier}>suppr premier article</button>
       <main>
         {articles.length > 0 && <>
           {articles.map((article, index) => {
--- redux/src/actions/article.js
+++ redux/src/actions/article.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
 export const GET_ALL_POSTS = "GET_ALL_POSTS" ;
+export const DELETE_FIRST = "DELETE_FIRST" ;
 
 export function getAllPost () {
     return (dispatch) => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then( ({data}) => dispatch({type : GET_ALL_POSTS , payload : data} ) )
     }
+}
+
+export function deleteFirst(){
+    return {type:DELETE_FIRST}
 }
